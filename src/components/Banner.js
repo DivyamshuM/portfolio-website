fce5c7ae0ff0d0602571480a8661fd6759230af9
DivyamshuM <divyamshu287@gmail.com>
2026-06-18 21:17:27 -0700

    changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,47 +1,8 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import 'animate.css';
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(200);
-  const toRotate = ["Full Stack Developer", "Software Engineer", "Web Developer"];
-  const period = 1500;
-
-  const tick = useCallback(() => {
-    const i = loopNum % toRotate.length;
-    const fullText = toRotate[i];
-    const updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(100);
-    } else {
-      setDelta(200);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setTimeout(() => {
-        setDelta(200);
-      }, period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-    }
-  }, [isDeleting, loopNum, text.length, toRotate, period]);
-
-  useEffect(() => {
-    const ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker); };
-  }, [delta, tick]);
-
   return (
     <section className="banner" id="home">
       <Container>
@@ -51,8 +12,6 @@ export const Banner = () => {
               Hi there!
               <br />
               I'm Divyamshu Mandadi
-              <br />
-              <span className="wrap" style={{ marginTop: '1em', display: 'block' }}>{text}</span>
             </h1>
           </Col>
           <Col xs={12} md={6} xl={5} className="d-flex justify-content-end">
